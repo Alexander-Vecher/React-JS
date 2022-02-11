aff1889e4ec98ad7c49827921353424050b52d3e
alexander-vecher <evening.sasha@mail.ru>
2022-02-11 22:42:22 +0300

    02

---
+++ src/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+    <div className="content">
+        <div><img
+            src=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn33l7Ax7MNugsH6pPwsP73xAn-4spGAdnzA&usqp=CAU"/>
+        </div>
+        <div>ava+description</div>
+        <div>
+            My posts
+            <div>New posts</div>
+
+            <div>post 1</div>
+            <div>post 2</div>
+
+
+        </div>
+    </div> )
+}
+export default Profile
