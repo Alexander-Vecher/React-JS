8059e2725130950934d3e85738c56d47704299bd
alexander-vecher <evening.sasha@mail.ru>
2022-02-20 23:01:27 +0300

    modules

---
+++ src/Header.module.css
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Header = () => {
+    return <header className="header">
+        <img
+            src="https://d1wnwqwep8qkqc.cloudfront.net/uploads/stage/stage_image/57440/optimized_large_thumb_stage.jpg"/>
+    </header>
+}
+
+export default Header
